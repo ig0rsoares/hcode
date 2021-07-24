d5b491affb6b1d858e551c488384706f3b0f3d33
ig0rsoares <igor_soares15@live.com>
2021-07-24 09:28:44 -0300

    subindo diretorio das aulas sobre variaveis

---
+++ variaveis/exemplo2.php
@@ -0,0 +1,20 @@
+<?php
+
+// variavel que captura o ano de nascimento
+$anoNascimento = 1990;
+
+// variavel que captura o nome completo
+$nome = "Igor Soares";
+$sobrenome = "Silveira";
+$nomecompleto = $nome." ".$sobrenome;
+
+echo $nomecompleto;
+
+if (isset($nome)) {
+
+    echo "<br> $nome" ;
+}
+
+
+
+?>
---
+++ variaveis/exemplo3.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
+ ///////////////////////////////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga");
+// echo $frutas[2];
+
+$nascimento = new DateTime();
+
+// var_dump($nascimento);
+
+$arquivo = fopen("exemplo3.php", "r");
+
+// var_dump($arquivo);
+
+$nulo = null;
+$vazio ="";
+
+$teste = array("igor", "2", "55.1");
+echo $teste[0];
+
+
+
+?>
